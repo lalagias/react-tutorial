aab2168f54a495722722a2e59692a08da64a2ef5
lalagias <jimkountanis@gmail.com>
2019-01-09 01:45:05 +0200

    New dependecy uuid for random id generator, add todo item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 import Header from './components/layout/Header';
+import uuid from 'uuid';
 
 import './App.css';
 
@@ -8,17 +10,17 @@ class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Dinner',
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Meeting with boss',
         completed: false
       },
@@ -43,14 +45,27 @@ class App extends Component {
     )]});
   }
 
+  // Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo]});
+  }
+
   render() {
     return (
       <div className="App">
-      <Header />
-        <Todos todos={this.state.todos}
-        markComplete={this.markComplete}
-        delTodo={this.delTodo}
-        />
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo} />
+          <Todos todos={this.state.todos}
+          markComplete={this.markComplete}
+          delTodo={this.delTodo}
+          />
+        </div>
       </div>
     );
   }
