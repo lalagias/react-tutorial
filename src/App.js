95bde1197047d303d7528a935fd94b5a865066c5
lalagias <jimkountanis@gmail.com>
2019-01-09 02:22:00 +0200

    Add PropTypes, new requests to JSONplaceholder

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import About from './components/pages/About';
 import Header from './components/layout/Header';
-import uuid from 'uuid';
+// import uuid from 'uuid';
 import axios from 'axios';
 
 import './App.css';
@@ -15,8 +15,8 @@ class App extends Component {
   }
 
   componentDidMount() {
-      axios.get('https://jsonplaceholder.typicode.com/todos')
-        .then(res => console.log(res.data));
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+        .then(res => this.setState({ todos: res.data }));
   }
 
   // Toggle Complete
@@ -32,19 +32,21 @@ class App extends Component {
 
   // Delete Todo
   delTodo = (id) => {
-    this.setState({ todos: [...this.state.todos.filter(todo => todo.id
-      !== id
-    )]});
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.setState({ todos: [...this.state.todos.filter
+        (todo => todo.id !== id)]}
+      )
+    );
   }
 
   // Add Todo
   addTodo = (title) => {
-    const newTodo = {
-      id: uuid.v4(),
+    axios.post('https://jsonplaceholder.typicode.com/todos', {
       title,
       completed: false
-    }
-    this.setState({ todos: [...this.state.todos, newTodo]});
+    })
+      .then(res => this.setState({ todos: [...this.state.todos, res.data]}));
+
   }
 
   render() {
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class AddTodo extends Component {
   state = {
@@ -30,11 +31,16 @@ export class AddTodo extends Component {
         value="Submit"
         className="btn"
         style={{ flex: '1' }}
-
         />
       </form>
     )
   }
+}
+
+
+// PropTypes
+AddTodo.propTypes = {
+  addTodo: PropTypes.func.isRequired,
 }
 
 export default AddTodo;
